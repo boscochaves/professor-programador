5a396910eec852771f9a047798a73143444c0867
bosco.pro <77576617+boscochaves@users.noreply.github.com>
2023-04-07 18:46:40 -0300

    Add files via upload

---
+++ prog002.cpp
@@ -0,0 +1,9 @@
+//prog02.cpp
+#include <iostream>
+#include <locale>
+using namespace std;
+int main(){
+     setlocale(LC_ALL, "");
+     cout << "Programa para somar dois números:\n";
+     cout<<5+7;
+}
